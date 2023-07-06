580011e4c111a9db10ad8dcc7a853ca3db53f1ec
Nguyen <Ken.Nguyen@campus.lmu.de>
2023-07-06 16:21:52 +0200

    fix snychro

--- OpenRDW/Assets/Own Scripts/PlayerSyncronization.cs
+++ OpenRDW/Assets/Own Scripts/PlayerSyncronization.cs
@@ -1,18 +1,12 @@
 using UnityEngine;
 using Valve.VR;
 
-public class PlayerSyncronization : MonoBehaviour
+public class PlayerSynchronization : MonoBehaviour
 {
     public Transform targetLocation; // The target location where you want the player to be
-
-    private Transform playerHead; // Reference to the player's head object (e.g., the camera)
+    public Transform playerHead; // Reference to the player's head object (e.g., the camera)
 
     private bool movePlayer = false;
-
-    private void Start()
-    {
-
-    }
 
     private void Update()
     {
@@ -22,20 +16,21 @@ public class PlayerSyncronization : MonoBehaviour
             playerHead = GameObject.Find("VR Player (Host)/Real/Head").transform;
             movePlayer = true;
         }
+
         if (movePlayer)
         {
-         // Calculate the offset between the current position and the target position
-        Vector3 offset = targetLocation.position - playerHead.position;
+            // Calculate the offset between the current position and the target position
+            Vector3 offset = targetLocation.position - playerHead.position;
 
-        // Apply the offset to the player's position
-        Transform realParent = GameObject.Find("VR Player (Host)/Real").transform; 
-        realParent.transform.position += offset;
+            // Apply the offset to the player's position
+            Transform realParent = GameObject.Find("VR Player (Host)/Real").transform;
+            realParent.transform.position += offset;
 
-        // Set the player's rotation to match the target rotation
-        realParent.transform.rotation = targetLocation.rotation;
+            // Set the player's rotation to match the target rotation
+            Quaternion headRotationDifference = Quaternion.Inverse(playerHead.rotation) * targetLocation.rotation;
+            realParent.transform.rotation *= headRotationDifference;
 
-        movePlayer = false;
+            movePlayer = false;
         }
-
     }
 }
