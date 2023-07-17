8eb47b71393ea2fd6c8c1fcff7c50abaeb559fc3
Nguyen <Ken.Nguyen@campus.lmu.de>
2023-07-17 17:17:08 +0200

    Bugfix midpoint implementation

--- OpenRDW/Assets/Own Scripts/AttachRedirectionTargets.cs
+++ OpenRDW/Assets/Own Scripts/AttachRedirectionTargets.cs
@@ -5,10 +5,10 @@ using HR_Toolkit.Redirection;
 
 public class AttachRedirectionTargets : MonoBehaviour
 { 
-
     public enum AttachMethod
     {
-        midpoint, otherHand
+        midpoint,
+        otherHand
     }
 
     public AttachMethod attachMethod;
@@ -16,45 +16,98 @@ public class AttachRedirectionTargets : MonoBehaviour
     private Transform realHandOfOtherPlayer;
     private Transform virtualHandOfOtherPlayer;
     private Transform ownRealHand;
+    private Transform realHandOfOtherPlayerVirtual;
+
     private bool attachObjects = false;
+    private bool isMidPointSet = false;
+
+
+    private GameObject otherPlayerHandObject;
     private GameObject redirectionTarget;
     private GameObject redirectedTarget;
-    // Start is called before the first frame update
+
+    private Vector3 initialMidpoint;
+    
     void Start()
+    {
+        FindObjects();
+    }
+
+    void Update()
+    {
+        HandleKeyPress();
+        HandleObjectAttachments();
+    }
+
+    void FindObjects()
     {
         redirectedTarget = GameObject.Find("Redirected Virtual Object");
         redirectionTarget = GameObject.Find("Redirected Real Target");
+        otherPlayerHandObject = GameObject.Find("OtherPlayerHandObject");
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    void HandleKeyPress()
     {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Debug.Log("Redirection Targets Attached");
-            GameObject vrPlayerGuest = GameObject.Find("VR Player (Guest)");
-            realHandOfOtherPlayer = vrPlayerGuest.transform.Find("Real/Right Hand"); ;
-            virtualHandOfOtherPlayer = vrPlayerGuest.transform.Find("Virtual/Right Hand");
-            ownRealHand = GameObject.Find("VR Player (Host)/Real/Right Hand").transform;
-            redirectedTarget.transform.Find("VirtualToRealConnection").GetComponent<VirtualToRealConnection>().enabled = true;
-            redirectedTarget.transform.Find("VirtualToRealConnection").GetComponent<VirtualToRealConnection>().realPosition = GameObject.Find("Real hand of Other Player").transform;
-            attachObjects = true;
-            if(attachMethod == AttachMethod.midpoint)
-            {
-                Vector3 midpoint = (realHandOfOtherPlayer.position + ownRealHand.position) * 0.5f;
+        if (!Input.GetKeyDown(KeyCode.C)) return;
+        
+        Debug.Log("Redirection Targets Attached");
+        realHandOfOtherPlayerVirtual = GameObject.Find("Real hand of Other Player").transform;
+        GameObject vrPlayerGuest = GameObject.Find("VR Player (Guest)");
+        realHandOfOtherPlayer = vrPlayerGuest.transform.Find("Real/Right Hand"); ;
+        virtualHandOfOtherPlayer = vrPlayerGuest.transform.Find("Virtual/Right Hand");
+        ownRealHand = GameObject.Find("Controller (right)").transform;
 
-                // Set the position of the objectToMove to the calculated midpoint position
-                redirectionTarget.transform.position = midpoint;
-            }
-        }
-        if (attachObjects)
+        EnableVirtualToRealConnection();
+
+        attachObjects = true;
+
+        if (attachMethod != AttachMethod.midpoint) return;
+
+        if (!isMidPointSet)
         {
-            redirectedTarget.transform.position = virtualHandOfOtherPlayer.position;
-            if(attachMethod == AttachMethod.otherHand)
-            {
-                redirectionTarget.transform.position = realHandOfOtherPlayer.position;
-            }
+            SetMidpoint();
+        }
+        else
+        {
+            ResetMidpoint();
         }
     }
 
+    void EnableVirtualToRealConnection()
+    {
+        var virtualToRealConnection = redirectedTarget.transform
+            .Find("VirtualToRealConnection").GetComponent<VirtualToRealConnection>();
+
+        virtualToRealConnection.enabled = true;
+        virtualToRealConnection.realPosition = redirectionTarget.transform;
+    }
+
+    void SetMidpoint()
+    {
+        Vector3 midpoint = (realHandOfOtherPlayerVirtual.position + ownRealHand.position) * 0.5f;
+        redirectionTarget.transform.position = midpoint;
+
+        initialMidpoint = midpoint;
+        isMidPointSet = true;
+    }
+
+    void ResetMidpoint()
+    {
+        redirectionTarget.transform.position = initialMidpoint;
+        isMidPointSet = false;
+    }
+
+    void HandleObjectAttachments()
+    {
+        if (!attachObjects) return;
+        
+        redirectedTarget.transform.position = virtualHandOfOtherPlayer.position;
+        otherPlayerHandObject.transform.position = realHandOfOtherPlayer.position;
+
+        if (attachMethod == AttachMethod.otherHand)
+        {
+            redirectionTarget.transform.position = realHandOfOtherPlayerVirtual.transform.position;
+        }
+    }
 }
