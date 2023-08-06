caae5c5656b3983a46d4c22cf4fb1d21136d20c7
Kenny-Minh Nguyen <kennyminh93@yahoo.de>
2023-08-06 18:51:43 +0200

    Realobjecttovirtual

--- OpenRDW/Assets/Own Scripts/RealObectToVirtual.cs
+++ OpenRDW/Assets/Own Scripts/RealObectToVirtual.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class RealObectToVirtual : MonoBehaviour
 {
-    
     public Transform virtualAvatar;
     public GameObject objectToClone;
     private Transform realAvatar;
@@ -15,22 +14,28 @@ public class RealObectToVirtual : MonoBehaviour
 
     void Start()
     {
-
+        // Subscribe to the X key press event from ConfigurationScript
+        ConfigurationScript.Instance.OnXKeyPressed += HandleXKeyPress;
     }
 
+    void OnDestroy() // Unsubscribe when the object is destroyed
+    {
+        ConfigurationScript.Instance.OnXKeyPressed -= HandleXKeyPress;
+    }
+
+    void HandleXKeyPress()
+    {
+        Debug.Log("Real Object now attached to virtual avatar");
+        // Spawn the clone at the same position as the object to clone
+        clone = Instantiate(objectToClone, objectToClone.transform.position, objectToClone.transform.rotation);
+        clone.name = "Real hand of Other Player";
+        realAvatar = ConfigurationScript.Instance.vrPlayerHost.transform.Find("Real/Head");
+        initialOffset = realAvatar.position - objectToClone.transform.position;
+        scriptEnabled = true;
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            Debug.Log("Real Object now attached to virtual avatar");
-                    // Spawn the clone at the same position as the object to clone
-            clone = Instantiate(objectToClone, objectToClone.transform.position, objectToClone.transform.rotation);
-            clone.name = "Real hand of Other Player";
-            realAvatar = GameObject.Find("VR Player (Host)").transform.Find("Real").transform.Find("Head");
-            initialOffset = realAvatar.position - objectToClone.transform.position;
-            scriptEnabled = true;
-        }
         if (scriptEnabled)
         {
             // Calculate the relative position of the real avatar's facing direction to the object
@@ -39,6 +44,5 @@ public class RealObectToVirtual : MonoBehaviour
             // Set the position of the clone based on the virtual avatar's position and the relative position
             clone.transform.position = virtualAvatar.position + (virtualAvatar.rotation * relativePosition);
         }
-
     }
 }
